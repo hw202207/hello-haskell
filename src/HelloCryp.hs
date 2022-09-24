a4727db930000c7d027027f810de207af8aceb0a
Haisheng W - M <haisheng@mercury.com>
2022-09-24 00:30:18 -0700

    another code verifier generator

--- src/HelloCryp.hs
+++ src/HelloCryp.hs
@@ -1,21 +1,26 @@
-{-# LANGUAGE OverloadedStrings, ImportQualifiedPost #-}
+{-# LANGUAGE ImportQualifiedPost #-}
+{-# LANGUAGE OverloadedStrings #-}
 
 module HelloCryp where
 
 import Control.Monad.IO.Class
 import Crypto.Hash qualified as H
-import Crypto.Random.Entropy
+import Crypto.Random qualified as Crypto
+import Data.ByteArray qualified as ByteArray
 import Data.ByteString
-import Data.ByteString.Base64.URL qualified as B64
 import Data.ByteString qualified as BS
+import Data.ByteString.Base64.URL qualified as B64
 import Data.ByteString.Char8 qualified as BS8
-import Data.Text.Lazy (Text, pack)
+import Data.Text (Text)
+import Data.Text qualified as T
+import Data.Text.Encoding qualified as T
+import Data.Word
 import Hedgehog.Gen qualified as Gen
 import Hedgehog.Range qualified as Gen
-import Data.ByteArray qualified as ByteArray
 
 cv :: String
 cv = "Lv6BZHaJDM9OZdCbraOnpufWUvxowHwu.IsG8NPlHMER_2WHvIfaqh4dcaphJmhS~OacldPaDCsfs2LVKBept0xG1jeS3TMFpBApAJ9etfRK-SFc.xmQrgag._leRNbS"
+
 cc = "GJeXEWdiwiKVpCbJqjwJWwVmr2evwt1JSs2YZBHgWSs"
 
 hashed :: ByteString
@@ -23,32 +28,23 @@ hashed = "189797116762c22295a426c9aa3c095b0566af67afc2dd494acd986411e0592b"
 
 main :: IO ()
 main = do
+  putStrLn "--- test case 1 ---"
   str <- genCodeVerifier
   let hashed = (hashSHA256 $ BS8.pack str)
-  let encoded = base64URLEncode $ BS.pack $ ByteArray.unpack hashed
+  let encoded = B64.encodeBase64Unpadded' . BS.pack . ByteArray.unpack . hashSHA256 . BS8.pack $ str
   putStrLn str
   print hashed
   print encoded
+  putStrLn "--- test case 2 ---"
+  str2 <- genCodeVerifier2
+  let hashed2 = hashSHA256 str2
+  let encoded2 = B64.encodeBase64Unpadded' . BS.pack . ByteArray.unpack . hashSHA256 $ str2
+  print (str2)
+  print hashed2
+  print encoded2
 
-pkceCodeChallenge :: String -> ByteString
-pkceCodeChallenge str =
-  let hashed = (hashSHA256 $ BS8.pack str)
-  in base64URLEncode $ BS.pack $ ByteArray.unpack hashed
-
-
--- hashSHA256 :: ByteString -> String
-hashSHA256 bs = (H.hash bs :: H.Digest H.SHA256)
-
--- base64URLEncode :: ByteString -> ByteString
-base64URLEncode =  B64.encodeBase64Unpadded'
-  -- BS.takeWhile notEqSign . BS.map urlSafeConvert . B64.encode
-  where
-    urlSafeConvert :: Char -> Char
-    urlSafeConvert '+' = '-'
-    urlSafeConvert '/' = '_'
-    urlSafeConvert x = x
-    notEqSign :: Char -> Bool
-    notEqSign = (/=) '='
+hashSHA256 :: ByteString -> H.Digest H.SHA256
+hashSHA256 = H.hash
 
 genCodeVerifier :: MonadIO m => m String
 genCodeVerifier =
@@ -56,9 +52,25 @@ genCodeVerifier =
     Gen.list (Gen.singleton 128) $
       Gen.frequency [(10, Gen.alphaNum), (1, Gen.element ['-', '_', '.', '~'])]
 
-genCodeVerifier2 :: IO ()
-genCodeVerifier2 = do
-  bs <- getEntropy 128
-  BS.putStr (bs :: ByteString)
-  print ""
-  print (BS.length bs)
+-- | the expected characters are following.
+-- The default 'getRandomBytes' generates bytes out of this scope.
+--
+-- @
+-- [A-Z] / [a-z] / [0-9] / "-" / "." / "_" / "~"
+-- @
+genCodeVerifier2 :: MonadIO m => m ByteString
+genCodeVerifier2 = liftIO (getBytesInternal BS.empty)
+
+getBytesInternal :: BS.ByteString -> IO BS.ByteString
+getBytesInternal ba
+  | BS.length ba >= 128 = pure (BS.take 128 ba)
+  | otherwise = do
+      bs <- Crypto.getRandomBytes 128
+      let bsUnreserved = ba `BS.append` BS.filter isUnreversed bs
+      getBytesInternal bsUnreserved
+
+isUnreversed :: Word8 -> Bool
+isUnreversed w = w `BS.elem` unreverseBS
+
+unreverseBS :: BS.ByteString
+unreverseBS = BS.pack $ [97 .. 122] ++ [65 .. 90] ++ [45, 46, 95, 126]
