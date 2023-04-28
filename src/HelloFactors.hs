b67c1206c2bdadd76cbaa4db50b3f53593116e62
Haisheng W - M <haisheng@mercury.com>
2023-04-28 15:16:52 -0700

    GHC 9.6

--- src/HelloFactors.hs
+++ src/HelloFactors.hs
@@ -3,7 +3,7 @@ module HelloFactors where
 -- | All possible factors of a number excluding itself
 factorOf :: Integer -> [Integer]
 factorOf n =
-    filter (/= n) $
+    -- filter (/= n) $
         concat $
             [[x, n `div` x] | x <- [1 .. sqrtIntegerVal n], n `rem` x == 0]
   where
@@ -12,9 +12,15 @@ factorOf n =
 
 -- | Any number between 300-399 that has 12 factors?
 run :: [(Integer, [Integer])]
-run = [(x, factorOf x) | x <- [300 .. 399], length (factorOf x) == 12]
+run = [(x, factorOf x) | x <- [300 .. 399], length (factorOf x) == 24]
+
+run2 :: [Integer]
+run2 = [ x | x <- [300..399], isDivideByAll x]
+
+isDivideByAll n = all (== 0) [ n `rem` x | x<- [1..12] ]
 
 main :: IO ()
 main = do
     print $ show $ factorOf 21
     mapM_ print run
+    mapM_ print run2
